66ac50bcf31b8ff87369de33dd37ea5618793127
juanvizuettefajardo <juanvizuette58@gmail.com>
2025-05-17 18:41:55 -0500

    first commit

---
+++ back/src/main/java/com/myproyect/model/Menu.java
@@ -0,0 +1,49 @@
+package com.myproyect.model;
+
+public class Menu {
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private double precio;
+
+    public Menu() {
+    }
+
+    public Menu(int id, String nombre, String descripcion, double precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+}
