5edb249fec3edbdf6bfb20aa9ca78f577a4f9081
juanvizuettefajardo <juanvizuette58@gmail.com>
2025-05-17 19:18:52 -0500

    fifth commit

--- back/src/main/java/com/myproyect/repository/ClienteRepository.java
+++ back/src/main/java/com/myproyect/repository/ClienteRepository.java
@@ -9,26 +9,31 @@ import java.util.List;
 import com.myproyect.model.Cliente;
 
 public class ClienteRepository {
-    public void guardar(Cliente cliente) throws SQLException {
+    private final Connection conn;
+
+    public ClienteRepository() throws SQLException {
+        this.conn = DatabaseConnection.getInstance();
+    }
+
+    public void insertar(Cliente c) throws SQLException {
         String sql = "INSERT INTO clientes(nombre,cedula, telefono,email) VALUES (?,?,?,?)";
         try (Connection conn = DatabaseConnection.getInstance(); PreparedStatement stmt = conn.prepareStatement(sql)) {
-            stmt.setString(1, cliente.getNombre());
-            stmt.setString(2, cliente.getCedula());
-            stmt.setString(3, cliente.getTelefono());
-            stmt.setString(4, cliente.getEmail());
+            stmt.setString(1, c.getNombre());
+            stmt.setString(2, c.getCedula());
+            stmt.setString(3, c.getTelefono());
+            stmt.setString(4, c.getEmail());
             stmt.executeUpdate();
         }
 
     }
 
     public List<Cliente> obtenerTodos() throws SQLException {
-        List<Cliente> clientes = new ArrayList<>();
-        String sql = "SELECT * FROM mesas";
-        try (Connection conn = DatabaseConnection.getInstance();
-                Statement stmt = conn.createStatement();
-                ResultSet rs = stmt.executeQuery(sql)) {
+        List<Cliente> list = new ArrayList<>();
+        String sql = "SELECT * FROM clientes";
+        try (Statement st = conn.createStatement();
+                ResultSet rs = st.executeQuery(sql)) {
             while (rs.next()) {
-                clientes.add(new Cliente(
+                list.add(new Cliente(
                         rs.getInt("id"),
                         rs.getString("nombre"),
                         rs.getString("cedula"),
@@ -36,38 +41,14 @@ public class ClienteRepository {
                         rs.getString("email")));
             }
         }
-        return clientes;
-    }
-
-    public void actualizar(Cliente cliente) throws SQLException {
-        String sql = "UPDATE clientes SET nombre = ?, cedula = ?, telefono = ?, email = ? WHERE id = ?";
-        try (Connection conn = DatabaseConnection.getInstance(); PreparedStatement stmt = conn.prepareStatement(sql)) {
-            stmt.setString(1, cliente.getNombre());
-            stmt.setString(2, cliente.getCedula());
-            stmt.setString(3, cliente.getTelefono());
-            stmt.setString(4, cliente.getEmail());
-            stmt.executeUpdate();
-        }
-    }
-
-    public void eliminar(int id) throws SQLException {
-        String sql = "DELETE clientes WHERE id = ?";
-        try (Connection conn = DatabaseConnection.getInstance(); PreparedStatement stmt = conn.prepareStatement(sql)) {
-            stmt.setInt(1, id);
-            stmt.executeUpdate();
-        }
-
-    }
-
-    public List<Cliente> findAll() throws SQLException {
-        return obtenerTodos();
+        return list;
     }
 
     public Cliente getById(int id) throws SQLException {
-        String sql = "SELECT * FROM  WHERE id = ?";
-        try (Connection conn = DatabaseConnection.getInstance(); PreparedStatement stmt = conn.prepareStatement(sql)) {
-            stmt.setInt(1, id);
-            try (ResultSet rs = stmt.executeQuery()) {
+        String sql = "SELECT * FROM  WHERE id=?";
+        try (PreparedStatement st = conn.prepareStatement(sql)) {
+            st.setInt(1, id);
+            try (ResultSet rs = st.executeQuery()) {
                 if (rs.next()) {
                     return new Cliente(
                             rs.getInt("id"),
@@ -83,4 +64,25 @@ public class ClienteRepository {
 
         return null;
     }
+
+    public void actualizar(Cliente c) throws SQLException {
+        String sql = "UPDATE clientes SET nombre=?, cedula=?, telefono=?, email=? WHERE id=?";
+        try (PreparedStatement st = conn.prepareStatement(sql)) {
+            st.setString(1, c.getNombre());
+            st.setString(2, c.getCedula());
+            st.setString(3, c.getTelefono());
+            st.setString(4, c.getEmail());
+            st.executeUpdate();
+        }
+    }
+
+    public void eliminar(int id) throws SQLException {
+        String sql = "DELETE clientes WHERE id=?";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+        }
+
+    }
+
 }
