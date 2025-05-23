68fa8c9e96cc6e824502ec6bf1d0c898c38d2f24
juanvizuettefajardo <juanvizuette58@gmail.com>
2025-05-22 20:13:05 -0500

    Update ConsultasAvanzadas.jsx

--- front/src/pages/ConsultasAvanzadas.jsx
+++ front/src/pages/ConsultasAvanzadas.jsx
@@ -1,17 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function ConsultasAvanzadas() {
   const links = [
-    { path: "/consultas/reservas-dia-hora", label: "Reservas por Dia y Hora" },
+    { path: "/consultas/reservas-dia-hora", label: "Reservas por Día y Hora" },
     {
       path: "/consultas/reservas-mesa-hora",
       label: "Reservas por Mesa y Hora",
     },
+    {
+      path: "/consultas/canceladas-ultimos-3-meses",
+      label: "Canceladas Últimos 3 Meses",
+    },
+    { path: "/consultas/clientes-frecuentes", label: "Clientes Frecuentes" },
+    {
+      path: "/consultas/porcentaje-canceladas",
+      label: "Porcentaje Canceladas",
+    },
   ];
+
   return (
     <div>
       <h1>Consultas Avanzadas</h1>
-      <ul></ul>
+      <ul>
+        {links.map((link, i) => (
+          <li key={i}>
+            <Link to={link.path}>{link.label}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
